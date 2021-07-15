d23b6c5a92e1a4bae3f2d90f08513ff41f3fd575
Junaid Asghar <junaidasghar862@gmail.com>
2021-07-15 18:43:45 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+
+// QUESTION # O7
+
+// var text = "Hyderabad"
+// document.write("City: " + text + "<br>")
+
+// for (i = 0; i <= text.length; i++) {
+//     if (text.slice(i, i + 9) === "Hyderabad") {
+//         text = text.slice(0,0) + "Islamabad";
+//     }
+// }
+// document.write("after replacement: " + text)
+
+
+// QUESTION # O8
+// var text = "Ali and Sami are bestfriends."
+// var text2 = "They play circket and football together.";
+// for (var a = 0; a < text.length; a++) {
+//     if (text.slice(a, a + 3) === "and") {
+//         text = text.slice(0, 4) + "&" + text.slice(a + 3);
+//     } else if (text2.slice(a, a + 3) === "and") {
+//         text2 = text2.slice(0, 18) + "&" + text2.slice(a + 3);
+//     }
+// }
+// document.write(text)
+// document.write(text2)
+
+
+
+// 69
+// 65
+
+
+
+
+
+
+
+
+
+
+
+
+
+
